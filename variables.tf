3ff138c2d78de6e6da707da18408d27fd8cffce7
Ward Boeckx <r0896010@student.thomasmore.be>
2024-12-06 11:22:58 +0100

    aanmaak subnet test

--- variables.tf
+++ variables.tf
@@ -21,72 +21,60 @@ variable "DD-vpc-CIDR" {
     description = "The CIDR block for the custom VPC"
 }
 
-# #variables for the declaration of the subnets 
-# variable "PublicSubnet1_CIDR" {
-#     default = "10.0.1.0/24"
-#     description = "CIDR block for Public Subnet 1"
-# }
-# variable "PublicSubnet2_CIDR" {
-#     default = "10.0.2.0/24"
-#     description = "CIDR block for Public Subnet 2"
-# }
-
-# variable "PrivateSubnet1_CIDR" {
-#     default = "10.0.10.0/24"
-#     description = "CIDR block for Private Subnet 1"
-# }
-
-# variable "PrivateSubnet2_CIDR" {
-#     default = "10.0.20.0/24"
-#     description = "CIDR block for Private Subnet 2"
-# }
-
-# variable "PrivateSubnet3_CIDR" {
-#     default = "10.0.30.0/24"
-#     description = "CIDR block for Private Subnet 3"
-# }
-
-# variable "PrivateSubnet4_CIDR" {
-#     default = "10.0.40.0/24"
-#     description = "CIDR block for Private Subnet 4"
-# }
-
-# variable "igw" {
-#     default = "LE-igw"
-#     description = "Internet gateway for the VPC"
-# }
-
-# #variables for the nat gateways and the Elastic IPs
-# variable "nat_gw" {
-#     default = "DD-vpc-NATGateway1"
-#     description = "NAT Gateway for Private Subnet"
-# }
-
-# variable "eip" {
-#     default = "LE-EIP1"
-#     description = "Elastic IP for NAT Gateway"
-# }
-
-# variable "nat_gw2" {
-#     default = "DD-vpc-NATGateway2"
-#     description = "NAT Gateway for Private Subnet"
-# }
-
-# variable "eip2" {
-#     default = "LE-EIP2"
-#     description = "Elastic IP for NAT Gateway"
-# }
-
-
-# variable "all_IPs" {
-#     default = "0.0.0.0/0"
-#     description = "All IPs on the internet"
-# }
-
-# #The execution policy
-# variable "AWS_LABROLE_ARN" {
-#   description = "the arn of my labrole execution role"
-# }
-# variable "name_db" {
-#   default = "ledb"
-# }
+ #variables for the declaration of the subnets 
+ variable "PublicSubnet1_CIDR" {
+     default = "10.0.1.0/24"
+     description = "CIDR block for Public Subnet 1"
+ }
+ variable "PublicSubnet2_CIDR" {
+     default = "10.0.2.0/24"
+    description = "CIDR block for Public Subnet 2"
+ }
+variable "PrivateSubnet1_CIDR" {
+     default = "10.0.10.0/24"
+     description = "CIDR block for Private Subnet 1"
+ }
+ variable "PrivateSubnet2_CIDR" {
+     default = "10.0.20.0/24"
+     description = "CIDR block for Private Subnet 2"
+ }
+ variable "PrivateSubnet3_CIDR" {
+     default = "10.0.30.0/24"
+     description = "CIDR block for Private Subnet 3"
+ }
+ variable "PrivateSubnet4_CIDR" {
+     default = "10.0.40.0/24"
+     description = "CIDR block for Private Subnet 4"
+ }
+ variable "igw" {
+     default = "LE-igw"
+     description = "Internet gateway for the VPC"
+ }
+ #variables for the nat gateways and the Elastic IPs
+ variable "nat_gw" {
+     default = "DD-vpc-NATGateway1"
+     description = "NAT Gateway for Private Subnet"
+ }
+ variable "eip" {
+     default = "LE-EIP1"
+     description = "Elastic IP for NAT Gateway"
+ }
+ variable "nat_gw2" {
+     default = "DD-vpc-NATGateway2"
+     description = "NAT Gateway for Private Subnet"
+ }
+ variable "eip2" {
+     default = "LE-EIP2"
+     description = "Elastic IP for NAT Gateway"
+ }
+ variable "all_IPs" {
+     default = "0.0.0.0/0"
+     description = "All IPs on the internet"
+ }
+ #The execution policy
+ variable "AWS_LABROLE_ARN" {
+   description = "the arn of my labrole execution role"
+ }
+ variable "name_db" {
+   default = "ledb"
+ }
