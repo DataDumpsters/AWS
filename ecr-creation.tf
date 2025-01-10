767b698e0ebc7ef49264e2895a4fc257efac7636
Ward Boeckx <r0896010@student.thomasmore.be>
2025-01-10 11:37:28 +0100

    test

--- ecr-creation.tf
+++ ecr-creation.tf
@@ -1,6 +1,6 @@
 #Creating the ECR repository
 resource "aws_ecr_repository" "my_repository" {
-  name = "le-repo"
+  name = "Data_Dumpsters"
   image_scanning_configuration {
     scan_on_push = true
   }
